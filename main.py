beda8dfc4842f16d7813990b5cb7079b5df2f89a
wltnkim <wltnkim>
2023-01-11 18:34:28 +0900

    auto_mkdir

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import cv2
+import glob
+import os
+
+input_files = glob.glob('img/*.jpg')
+
+for input_file in input_files:
+    input_filename = input_file[4:-4]
+    img = cv2.imread(input_file)
+
+    folder_directory = "result/" + str(input_filename)
+    if not os.path.exists(folder_directory):
+        os.makedirs(folder_directory)
 
 
-img = cv2.imread('img/bus.jpg')
-num_rows,num_cols = img.shape[:2]
+    num_rows,num_cols = img.shape[:2]
+    for i in range(1, 361):
+        #print(input_file, i)
+        rotation_matrix = cv2.getRotationMatrix2D((num_cols/2,num_rows/2), i, 1)
+        img_rotation = cv2.warpAffine(img,rotation_matrix,(num_cols,num_rows))
+
+
+        filename = folder_directory + "/"+ str(input_filename) + "_" + str(i) + ".jpg"
+        #print(filename)
+        cv2.imwrite(filename, img_rotation)
+
+'''
 for i in range(1, 361):
-    rotation_matrix = cv2.getRotationMatrix2D((num_cols/2,num_rows/2), i, 1) # 가운데를 중심점으로 30도 돌리고 스케일 요소 0.7
+    rotation_matrix = cv2.getRotationMatrix2D((num_cols/2,num_rows/2), i, 1)
     img_rotation = cv2.warpAffine(img,rotation_matrix,(num_cols,num_rows))
-    cv2.imwrite("img/result/rotate_img" + str(i) + ".jpg", img_rotation)
-
+    cv2.imwrite("result/rotate_img" + str(i) + ".jpg", img_rotation)
+'''
 #cv2.imshow("Rotate result", img_rotation)
 #cv2.waitKey(0)
 
