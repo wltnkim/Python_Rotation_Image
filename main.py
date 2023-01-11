71c7e951460295d7f32fcf515b4e2fab722c82f6
wltnkim <wltnkim>
2023-01-11 18:48:36 +0900

    modify details

--- main.py
+++ main.py
@@ -15,21 +15,8 @@ for input_file in input_files:
 
     num_rows,num_cols = img.shape[:2]
     for i in range(1, 361):
-        #print(input_file, i)
         rotation_matrix = cv2.getRotationMatrix2D((num_cols/2,num_rows/2), i, 1)
         img_rotation = cv2.warpAffine(img,rotation_matrix,(num_cols,num_rows))
 
-
         filename = folder_directory + "/"+ str(input_filename) + "_" + str(i) + ".jpg"
-        #print(filename)
         cv2.imwrite(filename, img_rotation)
-
-'''
-for i in range(1, 361):
-    rotation_matrix = cv2.getRotationMatrix2D((num_cols/2,num_rows/2), i, 1)
-    img_rotation = cv2.warpAffine(img,rotation_matrix,(num_cols,num_rows))
-    cv2.imwrite("result/rotate_img" + str(i) + ".jpg", img_rotation)
-'''
-#cv2.imshow("Rotate result", img_rotation)
-#cv2.waitKey(0)
-
